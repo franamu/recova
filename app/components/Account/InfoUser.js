3255096e17f7b550c8d7ade16e386418f1ace3c6
Francisco <franamuchastegui@gmail.com>
2019-12-22 22:21:14 -0300

    empiezo a trabajar con el usuario

---
+++ app/components/Account/InfoUser.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {Avatar} from 'react-native-elements';
+
+export default function InfoUser(props) {
+  console.log(props);
+  return (
+    <View>
+      <Text>Info User</Text>
+    </View>
+  );
+}
